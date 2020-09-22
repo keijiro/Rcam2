25c342a58fbc621d5ede13d0422add17d517962d
Keijiro Takahashi <keijiro@gmail.com>
2020-09-23 01:11:51 +0900

    Visualizer: Recolor and Feedback

--- RcamVisualizer/Assets/Script/CustomPass/RcamRecolorController.cs
+++ RcamVisualizer/Assets/Script/CustomPass/RcamRecolorController.cs
@@ -10,28 +10,68 @@ sealed class RcamRecolorController : MonoBehaviour
 {
     #region Editable attributes
 
-    [Space]
-    [SerializeField] Gradient _backGradient = null;
-    [SerializeField] Gradient _frontGradient = null;
-    [SerializeField, Range(0, 1)] float _backOpacity = 0;
-    [SerializeField, Range(0, 1)] float _frontOpacity = 0;
     [SerializeField, Range(0, 1)] float _dithering = 0.5f;
-    [Space]
-    [SerializeField] Color _lineColor = Color.black;
     [SerializeField] float _lineThreshold = 0.5f;
     [SerializeField] float _lineContrast = 1;
 
     #endregion
 
-    #region Public properties
+    #region Public members
 
     public bool IsActive => _backOpacity > 0 || _frontOpacity > 0;
+    public bool BackFill { get; set; }
+    public bool FrontFill { get; set; }
+    public void ShuffleColors() => RandomizeGradientsAndColors();
 
-    public float BackOpacity
-      { get => _backOpacity; set => _backOpacity = value; }
+    #endregion
 
-    public float FrontOpacity
-      { get => _frontOpacity; set => _frontOpacity = value; }
+    #region Private variables
+
+    float _backOpacity;
+    float _frontOpacity;
+
+    Gradient _backGradient = new Gradient();
+    Gradient _frontGradient = new Gradient();
+    Color _lineColor;
+
+    #endregion
+
+    #region Random color scheme
+
+    GradientColorKey [] _colorKeys = new GradientColorKey[3];
+    GradientAlphaKey [] _alphaKeys =
+      new GradientAlphaKey[] { new GradientAlphaKey(1, 0) };
+
+    void RandomizeGradientsAndColors()
+    {
+        var h1 = Random.value;
+        var h2 = (h1 + 0.333f) % 1;
+
+        var h3 = Random.value;
+        var h4 = (h3 + 0.333f) % 1;
+
+        var bg1 = Color.black;
+        var bg2 = Color.HSVToRGB(h1, 1, 0.5f);
+        var bg3 = Color.HSVToRGB(h2, 1, 0.8f);
+
+        var fg1 = Color.HSVToRGB(h3, 1, 0.3f);
+        var fg2 = Color.HSVToRGB(h4, 1, 1.0f);
+        var fg3 = Color.white;
+
+        _colorKeys[0] = new GradientColorKey(bg1, 0.5f);
+        _colorKeys[1] = new GradientColorKey(bg2, 0.75f);
+        _colorKeys[2] = new GradientColorKey(bg3, 1);
+        _backGradient.SetKeys(_colorKeys, _alphaKeys);
+        _backGradient.mode = GradientMode.Fixed;
+
+        _colorKeys[0] = new GradientColorKey(fg1, 0.5f);
+        _colorKeys[1] = new GradientColorKey(fg2, 0.75f);
+        _colorKeys[2] = new GradientColorKey(fg3, 1);
+        _frontGradient.SetKeys(_colorKeys, _alphaKeys);
+        _frontGradient.mode = GradientMode.Fixed;
+
+        _lineColor = Color.white;
+    }
 
     #endregion
 
@@ -56,6 +96,20 @@ sealed class RcamRecolorController : MonoBehaviour
         _props.SetVector("_LineParams", lineParams);
 
         return _props;
+    }
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+      => RandomizeGradientsAndColors();
+
+    void Update()
+    {
+        var delta = Time.deltaTime * 10;
+         _backOpacity = Mathf.Clamp01( _backOpacity + ( BackFill ? 1 : -1) * delta);
+        _frontOpacity = Mathf.Clamp01(_frontOpacity + (FrontFill ? 1 : -1) * delta);
     }
 
     #endregion
--- RcamVisualizer/Assets/Script/Misc/FeedbackEffectControllerAdapter.cs
+++ RcamVisualizer/Assets/Script/Misc/FeedbackEffectControllerAdapter.cs
@@ -9,33 +9,29 @@ sealed class FeedbackEffectControllerAdapter : MonoBehaviour
 
     [SerializeField] FeedbackEffectController _target = null;
     [SerializeField] Gradient _tintGradient = null;
+    [SerializeField] AnimationCurve _hueShiftCurve = null;
     [SerializeField] AnimationCurve _scaleCurve = null;
     [SerializeField] AnimationCurve _rotationCurve = null;
     [SerializeField] AnimationCurve _offsetCurve = null;
 
     #endregion
 
-    #region Public properties
-
-    public float Throttle { set => SetThrottle(value); }
-    public float Rotation { set => SetRotation(value); }
-    public float Offset { set => SetOffset(value); }
-
-    #endregion
-
     #region Accessor implementation
 
-    void SetThrottle(float value)
+    public void SetThrottle(float value)
     {
         _target.scale = _scaleCurve.Evaluate(value);
         _target.tint = _tintGradient.Evaluate(value);
         _target.enabled = value > 0.01f;
     }
 
-    void SetRotation(float value)
+    public void SetHueShift(float value)
+      => _target.hueShift = _hueShiftCurve.Evaluate(value);
+
+    public void SetRotation(float value)
       => _target.rotation = _rotationCurve.Evaluate(value);
 
-    void SetOffset(float value)
+    public void SetOffset(float value)
       => _target.offsetX = _offsetCurve.Evaluate(value);
 
     #endregion
