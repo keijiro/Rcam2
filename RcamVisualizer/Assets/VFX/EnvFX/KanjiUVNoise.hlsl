1bf8473a52b3746f5512f3319ed6c6d0c5805c39
Keijiro Takahashi <keijiro@gmail.com>
2020-09-23 18:25:58 +0900

    Add Kanji VFX

---
+++ RcamVisualizer/Assets/VFX/EnvFX/KanjiUVNoise.hlsl
@@ -0,0 +1,9 @@
+#include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise3D.hlsl"
+
+void KanjiUVNoise_float
+  (float2 uv, float frequency, float speed, float amplitude,
+   out float2 output)
+{
+    float3 np = float3(uv * frequency, _Time.y * speed);
+    output = snoise_grad(np).xy * amplitude;
+}
